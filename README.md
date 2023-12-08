6e27bc64abd2299c60d376fc2a96d36309dde0e8
Liror-24 <liror2401@gmail.com>
2023-12-08 10:45:06 +0100

    Deawoo - not use Fourier

---
+++ README.md
@@ -0,0 +1,17 @@
+## Deawoo
+
+### Métodos
+
+- Se han usado descriptores geométricos y de posicionamiento relativo entre componentes
+- Fourier daba problemas porque las letras D, C, O se confundían con el logo
+
+### To do
+
+- Separar mejor bordes (casos de imágenes con fondo o como imagen 74)
+- Segundo logo (imagen 82)
+- Limpiar código y optimizar
+- Limpiar imágenes. Si hay impurezas en el borde, se lía
+
+### Progreso
+
+- Si el logo está claro, lo detecta bastante bien
--- practica.m
+++ practica.m
@@ -21,44 +21,16 @@ markers(2:end-1,2:end-1) = 0;
 im_bw = ~imreconstruct(markers, double(~cont_bw),4);
 figure, imshow(im_bw), title('reconstrucció bw')
 
-% Dades varies. Potser utilitzar solidity
+% Dades varies
 dades = regionprops(im_bw, 'all');
-cc = bwconncomp(im_bw, 4);
+relativeHorizontalDistance = abs(dades(2).Centroid(1)-dades(3).Centroid(1))/abs(dades(2).Centroid(1)-dades(1).Centroid(1));
+relativeDistance23 = pdist2(dades(2).Centroid, dades(3).Centroid)^2/dades(2).Area;
+relativeArea = dades(1).Area/dades(3).Area;
 
-% Find Fourier descriptors
 ero = imerode(im_bw, strel('disk',1));
 new_cont = imsubtract(im_bw, ero);
 labeledImage = bwlabel(new_cont);
 figure, imshow(labeledImage, []), colormap('colorcube'), impixelinfo
-
-
-N = 10;
-fourierDescriptors = zeros(max(labeledImage(:)), N);
-for i = 1:max(labeledImage(:))
-    % Find descrete coordinates
-    [fila, col] = find(labeledImage==i);
-    [~,ind] = min(fila);
-    fila = fila(ind); col = col(ind);
-
-    B = bwtraceboundary(new_cont, [fila, col], 'E');
-
-    mig = mean(B);
-    Bc = B - mig;
-    s = Bc(:,1) + 1i*Bc(:,2);
-    z = fft(s);
-
-    tmp = z;
-    %tmp = (10000*z)/z(2)
-    tmp(N+1:end-N) = 0;
-    fourierDescriptors(i, :) = z(1:N)/z(2);
-
-    ss = ifft(tmp);
-    aux = zeros(300);
-    files = round(real(ss) + 150);
-    cols = round(imag(ss) + 150);
-    aux(sub2ind(size(aux),files,cols)) = 1;
-    figure, imshow(aux), title('image with 10 descriptors')
-end
 
 
 %% 
@@ -82,9 +54,8 @@ markers(2:end-1,2:end-1) = 0;
 im_bw = ~imreconstruct(markers, double(~cont_bw),4);
 figure, imshow(im_bw), title('reconstrucció bw')
 
-% Dades varies. Potser utilitzar solidity
-dades = regionprops(im_bw, 'all');
-cc = bwconncomp(im_bw, 4);
+% Dades varies
+dadesNoves = regionprops(im_bw, 'all');
 
 % Find Fourier descriptors
 ero = imerode(im_bw, strel('disk',1));
@@ -92,49 +63,40 @@ new_cont = imsubtract(im_bw, ero);
 labeledImage = bwlabel(new_cont);
 figure, imshow(labeledImage, []), colormap('colorcube'), impixelinfo
 
+L = max(labeledImage(:));
 
-N = 10;
-newFourierDescriptors = zeros(max(labeledImage(:)), N);
-for i = 1:max(labeledImage(:))
-    % Find descrete coordinates
-    [fila, col] = find(labeledImage==i);
-    [~,ind] = min(fila);
-    fila = fila(ind); col = col(ind);
-    B = bwtraceboundary(new_cont, [fila, col], 'E');
 
-    mig = mean(B);
-    Bc = B - mig;
-    s = Bc(:,1) + 1i*Bc(:,2);
-    z = fft(s);
-
-    tmp = z;
-    tmp(N+1:end-N) = 0;
-    newFourierDescriptors(i, :) = z(1:N)/z(2);
-
-    ss = ifft(tmp);
-    aux = zeros(500);
-    files = round(real(ss) + 250);
-    cols = round(imag(ss) + 250);
-    aux(sub2ind(size(aux),files,cols)) = 1;
-    figure, imshow(aux), title('image with 10 descriptors')
-end
-
-found = [];
-for i=1:max(labeledImage(:))
-    div = newFourierDescriptors(i,2);
-    for j = 1:3
-        aux = fourierDescriptors(j,:) - fourierDescriptors(j,2)*newFourierDescriptors(i,:)/div;
-        for k = 1:N
-            aux(k) = abs(aux(k));
+for i = 1:L
+    for j = 1:L
+        if j ~= i
+            for k = 1:L
+                if k ~= i & k ~= j
+                    newRelativeDistance = pdist2(dadesNoves(j).Centroid, dadesNoves(k).Centroid)/pdist2(dadesNoves(j).Centroid,dadesNoves(i).Centroid);
+                    newRelativeHorizontalDistance = abs(dadesNoves(j).Centroid(1)-dadesNoves(k).Centroid(1))/abs(dadesNoves(j).Centroid(1)-dadesNoves(i).Centroid(1));
+                    newRelativeDistance23 = pdist2(dadesNoves(j).Centroid, dadesNoves(k).Centroid)^2/dadesNoves(j).Area;
+                    newRelativeDistance12 = dadesNoves(i).Area/abs(dadesNoves(i).Centroid(2)-dadesNoves(j).Centroid(2))^2;
+                    newRelativeArea = dadesNoves(i).Area/dadesNoves(k).Area;
+                    if abs(newRelativeArea-relativeArea) < 8 & abs(newRelativeHorizontalDistance-relativeHorizontalDistance) < 0.1 & abs(newRelativeDistance23 - relativeDistance23) < 0.11 & ...
+                            abs(dadesNoves(i).Solidity - dades(1).Solidity) < 0.15 &...
+                            abs(dadesNoves(j).Circularity - dades(2).Circularity) < 0.15 & abs(dadesNoves(j).Solidity - dades(2).Solidity) < 0.15 &...
+                            abs(dadesNoves(k).Solidity - dades(3).Solidity) < 0.15
+                        [i,j,k]
+                        %{
+                        newRelativeDistance
+                        newRelativeHorizontalDistance
+                        newRelativeDistance23
+                        newRelativeDistance12
+                        newRelativeArea
+                        %}
+                    end
+                end
+            end
         end
-        if max(aux) < 0.05 & ~ ismember(j,found)
-            found = [found j]
-        end
+       
     end
 end
 
 
-%% 
 
 
 
