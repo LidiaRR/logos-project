a16a11af8576047e083bb54596b11d353b8033eb
PabloRodriguezElvira <pablo.rodriguez.elvira@estudiantat.upc.edu>
2023-12-19 23:54:32 +0100

    Añadida función de preprocesado y cambios en la estructura del git

---
+++ main_code/preprocess_image.m
@@ -0,0 +1,15 @@
+function im_result = preprocess_image(im_grey)
+    % Ajustar niveles de intensidad:
+    im_grey = imadjust(im_grey);
+    %figure,imshow(im_grey),title('adjusted image')
+
+    % Reconstruir imagen:
+    mark = im_grey;
+    mark(2:end-1,2:end-1) = 0;
+    im_reconstruct = imreconstruct(mark,im_grey);
+    %figure,imshow(rec),title('imagen reconstruida'),impixelinfo
+
+    % Binarizar con umbral de Otsu:
+    threshold = graythresh(im_reconstruct);
+    im_result = imbinarize(im_reconstruct,threshold);
+end
